cb668f9b670788ec6404a2cdbc8b3a1c5539e9a8
pixelboogie <41452340+pixelboogie@users.noreply.github.com>
2021-03-26 15:20:29 -0700

    Add files via upload

---
+++ GameManager.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+                Debug.Log("GameManager is NULL");
+
+                return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void DisplayName()
+    {
+        Debug.Log("My name is Dude.");
+    }
+}
---
+++ Player.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void Start()
+    {
+        GameManager.Instance.DisplayName();
+    }
+
+
+}
